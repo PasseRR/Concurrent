78da3778fc012e030afc705110a89c9c4709323b
PasseRR <xie__hai@sina.com>
2015-04-29 13:42:05 +0800

    atomic基本类型

--- src/com/utoken/concurrent/atomic/AtomicBaseTest.java
+++ src/com/utoken/concurrent/atomic/AtomicBaseTest.java
@@ -2,31 +2,83 @@ package com.utoken.concurrent.atomic;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * AtomicInteger Test
+ * atomic基本类型：AtomicInteger、AtomicLong、AtomicBoolean
  * @author xiehai
  * @version 1.0
  * @date 2015年4月29日上午10:10:22
  */
 public class AtomicBaseTest {
 	public static void main(String[] args) {
+		testAtomicBoolean();
+	}
+	
+	/**
+	 * 测试AtomicInteger
+	 */
+	public static void testAtomicInteger(){
 		ExecutorService es = Executors.newFixedThreadPool(10);
-		TestAtomicIntegerThread tat = new TestAtomicIntegerThread();
+		AtomicIntegerThread ait = new AtomicIntegerThread();
 		for(int i = 0; i < 100; ++i){
-			es.submit(tat);
+			es.submit(ait);
 		}
 		es.shutdown();
 	}
 	
-	static class TestAtomicIntegerThread implements Runnable{
+	/**
+	 * 测试AtomicLong
+	 */
+	public static void testAtomicLong(){
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		AtomicLongThread alt = new AtomicLongThread();
+		for(int i = 0; i < 100; ++i){
+			es.submit(alt);
+		}
+		es.shutdown();
+	}
+	
+	/**
+	 * 测试AtomicBoolean
+	 */
+	public static void testAtomicBoolean(){
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		AtomicBooleanThread alt = new AtomicBooleanThread();
+		for(int i = 0; i < 100; ++i){
+			es.submit(alt);
+		}
+		es.shutdown();
+	}
+	
+	static class AtomicIntegerThread implements Runnable{
 		
 		private AtomicInteger atomicInteger = new AtomicInteger(1);
 		
 		@Override
 		public void run() {
 			System.out.println(Thread.currentThread().getName() + " value = " + atomicInteger.getAndIncrement());
+		}
+	}
+	
+	static class AtomicLongThread implements Runnable{
+		private AtomicLong atomicLong = new AtomicLong(1);
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + " value = " + atomicLong.getAndIncrement());
+		}
+	}
+	
+	static class AtomicBooleanThread implements Runnable{
+		private AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+		@Override
+		public void run() {
+			boolean flg = atomicBoolean.get();
+			atomicBoolean.set(!flg);
+			System.out.println(Thread.currentThread().getName() + " value = " + flg);
+
 		}
 	}
 }
